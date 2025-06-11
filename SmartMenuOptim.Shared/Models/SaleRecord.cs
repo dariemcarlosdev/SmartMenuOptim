a6e1baf023dcb0ff2a8345cb71db7e697840166d
dariemcarlosdev <softevolutionsl@gmail.com>
2025-06-10 21:29:31 -0400

    [Refactor] Initialize SaleDate with current UTC time by default, according to Postgres requirement.

--- SmartMenuOptim.Shared/Models/SaleRecord.cs
+++ SmartMenuOptim.Shared/Models/SaleRecord.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string DishName { get; set; }
         public int QuantitySold { get; set; }
-        public DateTime SaleDate { get; set; }
+        public DateTime SaleDate { get; set; } = DateTime.UtcNow;
     }
 }
