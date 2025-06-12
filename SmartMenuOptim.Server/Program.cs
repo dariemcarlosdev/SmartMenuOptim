1cc6a20d7ef2eb30e3a704c137be923d0749829e
dariemcarlosdev <softevolutionsl@gmail.com>
2025-06-11 21:53:07 -0400

    [Refactor] Add HTTP client and CORS policy for API integration

--- SmartMenuOptim.Server/Program.cs
+++ SmartMenuOptim.Server/Program.cs
@@ -9,6 +9,25 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddScoped<IAIService, AIService>();
 
+// Add httpClient for external API calls
+builder.Services.AddHttpClient("BackendAPI",client =>
+{
+    client.BaseAddress = new Uri("https://localhost:5001/");
+});
+
+//add CORS policy to allow cross-origin requests from the frontend
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
+
+builder.Services.AddScoped<ISaleRecordService, SaleRecordService>();
+builder.Services.AddScoped<IReviewService, ReviewService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,7 +40,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
+app.UseCors("AllowAllOrigins");
 app.UseAntiforgery();
 
 app.MapStaticAssets();
