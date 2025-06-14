4c62dddc392e6c96d6b91b586abf4a2184d218a4
dariemcarlosdev <softevolutionsl@gmail.com>
2025-06-14 12:42:02 -0400

    [Refactor] Refactor service registrations in Program.cs
    Added new service registrations for ISaleRecordService, IReviewService, and logging. Removed previous registrations for ISaleRecordService and IReviewService to streamline the dependency injection setup. Configured logging to include console output.

--- SmartMenuOptim.Server/Program.cs
+++ SmartMenuOptim.Server/Program.cs
@@ -8,6 +8,9 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddScoped<IAIService, AIService>();
+builder.Services.AddScoped<ISaleRecordService, SaleRecordService>();
+builder.Services.AddScoped<IReviewService, ReviewService>();
+builder.Services.AddLogging();
 
 // Add httpClient for external API calls
 builder.Services.AddHttpClient("BackendAPI",client =>
@@ -15,9 +18,7 @@ builder.Services.AddHttpClient("BackendAPI",client =>
     client.BaseAddress = new Uri("https://localhost:7119/");
 });
 
-builder.Services.AddScoped<ISaleRecordService, SaleRecordService>();
-builder.Services.AddScoped<IReviewService, ReviewService>();
-
+builder.Logging.AddConsole();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
