c6fa04a581f131f138ac336bf3407bc159cca4e4
dariemcarlosdev <softevolutionsl@gmail.com>
2025-06-19 20:06:45 -0400

    [Refactor] Enhance IRepository with LINQ support and documentation
    Updated IRepository<T> to include Query() method for LINQ queries.
    Added XML documentation for methods in IRepository.cs and Repository.cs.
    Refactored GetByIdAsync to simplify code by removing AsTask().

--- SmartMenuOptim.Shared/Data/Interfaces/IRepository.cs
+++ SmartMenuOptim.Shared/Data/Interfaces/IRepository.cs
@@ -8,11 +8,44 @@ namespace SmartMenuOptim.Shared.Data.Interfaces
 {
     public interface IRepository<T> where T : class
     {
+        /// <summary>
+        /// This method allows LINQ queries on the repository, enabling filtering, sorting, and other LINQ operations directly on the repository.
+        /// It is useful for scenarios where you want to perform complex queries without loading all entities into memory first.
+        /// </summary>
+        /// <returns>returns an IQueryable<T></returns>
+        IQueryable<T> Query(); // Add this line to allow LINQ queries on the repository.
+        /// <summary>
+        /// This method retrieves an entity of type T by its ID asynchronously.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Task<T?> GetByIdAsync(int id);
+        /// <summary>
+        /// This method retrieves all entities of type T from the database asynchronously.
+        /// </summary>
+        /// <returns></returns>
         Task<IEnumerable<T>> GetAllAsync();
+        /// <summary>
+        /// This method adds a new entity of type T to the database asynchronously.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         Task AddAsync(T entity);
+        /// <summary>
+        /// This method updates an existing entity of type T in the database.
+        /// </summary>
+        /// <param name="entity"></param>
         void Update(T entity);
+        /// <summary>
+        /// This method deletes an entity of type T from the database.
+        /// </summary>
+        /// <param name="entity"></param>
         void Delete(T entity);
+        /// <summary>
+        /// This method checks if an entity of type T exists in the database by its ID asynchronously.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         Task<bool> ExistsAsync(int id);
     }
 }
--- SmartMenuOptim.Shared/Data/Repositories/Repository.cs
+++ SmartMenuOptim.Shared/Data/Repositories/Repository.cs
@@ -20,10 +20,19 @@ namespace SmartMenuOptim.Shared.Data.Repositories
             _dbSet = _context.Set<T>();
         }
 
+        /// <summary>
+        /// This method adds a new entity of type T to the database asynchronously.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         public async Task AddAsync(T entity) =>
                 await _dbSet.AddAsync(entity);
 
-
+        /// <summary>
+        /// This method deletes an entity of type T from the database.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public void Delete(T entity)
         {
             if (entity == null)
@@ -33,19 +42,42 @@ namespace SmartMenuOptim.Shared.Data.Repositories
             _dbSet.Remove(entity);
         }
 
-
+        /// <summary>
+        /// This method checks if an entity of type T exists in the database by its ID asynchronously.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public Task<bool> ExistsAsync(int id) =>
             _dbSet.AnyAsync(e => EF.Property<int>(e, "Id") == id);
 
-
+        /// <summary>
+        /// This method retrieves all entities of type T from the database asynchronously.
+        /// </summary>
         public async Task<IEnumerable<T>> GetAllAsync() =>
             await _dbSet.ToListAsync();
 
-
+        /// <summary>
+        /// This method retrieves an entity of type T by its ID asynchronously.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<T?> GetByIdAsync(int id) =>
-           await _dbSet.FindAsync(id).AsTask();
+           await _dbSet.FindAsync(id);
 
+        /// <summary>
+        /// This method allows LINQ queries on the repository, enabling filtering, sorting, and other LINQ operations directly on the repository.
+        /// </summary>
+        /// <returns></returns>
+        public IQueryable<T> Query()
+        {
+            return _dbSet.AsQueryable();
+        }
 
+        /// <summary>
+        /// This method updates an existing entity of type T in the database.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public void Update(T entity)
         {
             if (entity == null)
