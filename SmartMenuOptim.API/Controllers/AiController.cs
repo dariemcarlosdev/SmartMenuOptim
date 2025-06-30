9da7aacaf3c519c58b0e805a2882f577e63aec96
dariemcarlosdev <softevolutionsl@gmail.com>
2025-06-30 08:25:26 -0400

    [Refactor] Improve comment clarity in AiController.cs. Change was bring from branch - GithubActions-Pipeline/hotfix-AiController
    Updated the comment for the validation logic in the Recommend method to enhance clarity and maintain consistency in the code documentation.

--- SmartMenuOptim.API/Controllers/AiController.cs
+++ SmartMenuOptim.API/Controllers/AiController.cs
@@ -117,7 +117,7 @@ namespace SmartMenuOptim.API.Controllers
         public ActionResult<AiRecomendationRequest> Recommend([FromBody] AiRecomendationRequest request)
         {
             Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(request));
-            // Validate the request
+            // Validating the request 
             if (request.SaleRecords == null || !request.SaleRecords.Any() || request.SaleRecords.Any(sr => sr == null))
             {
                 return BadRequest("Sale records cannot be empty.");
