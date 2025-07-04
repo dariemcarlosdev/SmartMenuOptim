0e0f002fa7665784f0d1aa6ded84b9944e5c930f
dariemcarlosdev <softevolutionsl@gmail.com>
2025-07-04 11:54:08 -0400

    [Update | Refactor ]Update configuration and logging for Backend API
    - Added `SmartMenuOptim.Server/appsettings.Development.json` to .gitignore.
    - Introduced comments in `Program.cs` for HTTP client setup and logging of the Backend API base URL.
    - Updated `appsettings.Production.json` to specify the production `BackendApi` base URL.

--- SmartMenuOptim.Server/Program.cs
+++ SmartMenuOptim.Server/Program.cs
@@ -28,8 +28,21 @@ public class Program
             client.BaseAddress = new Uri(baseUrl);
         });
 
+        // Add httpClient for external API calls
+        //builder.Services.AddHttpClient("BackendAPI", (serviceProvider, client) =>
+        //{
+        //    var config = serviceProvider.GetRequiredService<IConfiguration>();
+        //    var baseUrl = config["BackendApi:BaseUrl"];
+        //    //client.BaseAddress = new Uri("https://localhost:7119/");
+        //    client.BaseAddress = new Uri(baseUrl);
+        //});
+
         builder.Logging.AddConsole();
         var app = builder.Build();
+
+        var config = app.Services.GetRequiredService<IConfiguration>();
+        var logger = app.Services.GetRequiredService<ILogger<Program>>();
+        logger.LogInformation("BackendAPI BaseUrl from config: {BaseUrl}", config["BackendApi:BaseUrl"]);
 
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
